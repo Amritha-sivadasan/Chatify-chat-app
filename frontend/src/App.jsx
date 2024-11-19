81f45cb6b778934cdb546e944c8ccd4488cc9cba
Amritha-sivadasan <amrithas172@gmail.com>
2024-11-19 19:21:26 +0530

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/chat" element={<Chat />} />
           </Routes>
-      
+       <p>kjfksjfkfse</p> 
      
     </div>
   );
